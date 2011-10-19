d6873dc8011a68f046eba4de32e647c53a808217
Patrick Reynolds <cpreynolds@gmail.com>
2011-10-18 23:13:45 -0400

    ENH: Using the requests library instead of urllib.
    It's a lot better.

--- communicator.py
+++ communicator.py
@@ -1,5 +1,5 @@
 import simplejson as json
-import urllib
+import requests as http
 from exceptions import PydasException
 
 class Communicator(object):
@@ -14,20 +14,27 @@ class Communicator(object):
         self.apiSuffix = '/api/json?method='
         self.serverUrl = url
 
-    def makeRequest(self, method):
+    def makeRequest(self, method, parameters=None):
         """
         Do the generic processing of a request to the server
         """
-        request = urllib.urlopen(self.serverUrl + self.apiSuffix + method)
-        code = request.getcode()
+        url = self.serverUrl + self.apiSuffix + method
+        request = None
+        if parameters:
+            request = http.post(url, headers=parameters)
+        else:
+            request = http.post(url, headers=parameters)
+        code = request.status_code
         if code != 200:
-            raise PydasException("Request failed with HTTP error code %d" % code)
+            raise PydasException("Request failed with HTTP error code "
+                                 "%d" % code)
 
-        response = json.loads(request.read())
+        response = json.loads(request.content)
 
         if response['stat'] != 'ok':
-            raise PydasException("Request failed with Midas error code %s: %s" % (response['code'],
-                                                                                  response['message']))
+            raise PydasException("Request failed with Midas error code "
+                                 "%s: %s" % (response['code'],
+                                             response['message']))
         return response['data']
         
     def getServerVersion(self):
@@ -36,3 +43,58 @@ class Communicator(object):
         """
         response = self.makeRequest('midas.version')
         return response['version']
+
+    def getServerInfo(self):
+        """
+        Get info from the server (this is an alias to getVersion on most
+        platforms, but it returns the whole dictionary).
+        """
+        response = self.makeRequest('midas.info')
+        return response
+
+    def getDefaultApiKey(self, email, password):
+        """
+        Gets the default api key given an email and password
+        """
+        parameters = dict()
+        parameters['email'] = email
+        parameters['password'] = password
+        response = self.makeRequest('midas.user.apikey.default', parameters)
+        return response['apikey']
+
+    def loginWithApiKey(self, email, apikey, application='Default'):
+        """
+        Login and get a token using an email and apikey. If you do not specify
+        a specific application, 'default' will be used
+        """
+        parameters = dict()
+        parameters['email'] = email
+        parameters['apikey'] = apikey
+        parameters['appname'] = application
+        response = self.makeRequest('midas.login', parameters)
+        return response['token']
+
+    def listUserFolders(self, token):
+        """
+        Use a users token to list the curent folders.
+        """
+        parameters = dict()
+        parameters['token'] = token
+        response = self.makeRequest('midas.user.folders', parameters)
+        return response
+    
+    def generateUploadToken(self, token, itemid, filename, checksum=None):
+        """
+        Generate a token to use for upload.
+        """
+        parameters = dict()
+        parameters['token'] = token
+        parameters['itemid'] = itemid
+        parameters['filename'] = filename
+        response = self.makeRequest('midas.upload.generatetoken', parameters)
+        return response
+
+    def createItem():
+        """
+        """
+        
