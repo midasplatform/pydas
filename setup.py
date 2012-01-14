fb226688e8de93a4d971e45ed387236e98639198
Patrick Reynolds <cpreynolds@gmail.com>
2012-01-14 10:24:09 -0500

    ENH: Fixing packaging.

--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
 from distutils.core import setup
 setup(name='pydas',
-      version='0.1.4',
-      package_dir={'pydas': 'src'},
+      version='0.1.6',
+      package_dir={'pydas': 'src/pydas'},
       packages=['pydas'],
       author='Patrick Reynolds',
       author_email='patrick.reynolds@kitware.com',
       url='http://github.com/cpatrick/pydas',
+      install_requires=['requests','simplejson']
       )
