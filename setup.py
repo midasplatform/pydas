7663eba39571b18421299d6946565f650b5e596f
Patrick Reynolds <cpreynolds@gmail.com>
2012-01-15 13:59:10 -0500

    ENH: Revised the version for a push up to pypi.

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from distutils.core import setup
 setup(name='pydas',
-      version='0.1.6',
+      version='0.1.7',
       package_dir={'pydas': 'src/pydas'},
       packages=['pydas'],
       author='Patrick Reynolds',
