cacf2e190224576be7bac4e5bb15589b35bcaec9
Patrick Reynolds <cpreynolds@gmail.com>
2012-03-14 17:31:56 -0400

    ENH: Retry on any error (sometimes it 500s).

--- pydas/retry.py
+++ pydas/retry.py
@@ -10,19 +10,16 @@ def reauth(fn):
     def wrapper(*args, **kw):
         try:
             return fn(*args, **kw)
-        except pydas.exceptions.PydasException as pe:
-            if pe.value.find('Invalid token') > -1:
-                # renew the token
-                # get the instance of the CoreDriver and set it as "that"
-                that = args[0]
-                token = that.login_with_api_key(that.__class__.email,
-                                                that.__class__.apikey)
-                # now fix up the arguments of the original call to use the renewed token
-                argsList = list(args)
-                argsList[2]['token'] = token
-                args = tuple(argsList)
-                # try the api call again
-                return fn(*args, **kw)
-            else:
-                raise pe
+        except pydas.exceptions.PydasException:
+            # renew the token
+            # get the instance of the CoreDriver and set it as "that"
+            that = args[0]
+            token = that.login_with_api_key(that.__class__.email,
+                                            that.__class__.apikey)
+            # now fix up the arguments of the original call to use the renewed token
+            argsList = list(args)
+            argsList[2]['token'] = token
+            args = tuple(argsList)
+            # try the api call again
+            return fn(*args, **kw)
     return wrapper
