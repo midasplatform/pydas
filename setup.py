5d38bfd991e58dee49e49d07525d5403c5fb2a73
Patrick Reynolds <cpreynolds@gmail.com>
2012-03-14 17:32:44 -0400

    ENH: Incrementing the version number.

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from distutils.core import setup
 setup(name='pydas',
-      version='0.2.0',
+      version='0.2.1',
       packages=['pydas'],
       author='Patrick Reynolds',
       author_email='patrick.reynolds@kitware.com',
