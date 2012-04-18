a478a7f8706d3798c6170d0a6874f0c6b616f89a
Michael Grauer <michael.grauer@kitware.com>
2012-04-18 12:03:05 -0400

    ENH: added a 30 second backoff before retrying a request.

--- pydas/retry.py
+++ pydas/retry.py
@@ -1,4 +1,5 @@
 import pydas.exceptions
+import time
 
 def reauth(fn):
     """
@@ -9,17 +10,24 @@ def reauth(fn):
     @wraps(fn)
     def wrapper(*args, **kw):
         try:
-            return fn(*args, **kw)
+            retVal = fn(*args, **kw)
+            return retVal
         except pydas.exceptions.PydasException:
+            # wait 30 seconds before retrying
+            time.sleep(30) 
+
             # renew the token
             # get the instance of the CoreDriver and set it as "that"
             that = args[0]
             token = that.login_with_api_key(that.__class__.email,
                                             that.__class__.apikey)
+
             # now fix up the arguments of the original call to use the renewed token
             argsList = list(args)
             argsList[2]['token'] = token
             args = tuple(argsList)
+
             # try the api call again
-            return fn(*args, **kw)
+            retVal = fn(*args, **kw)
+            return retVal
     return wrapper
