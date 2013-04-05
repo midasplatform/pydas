feab4730f48974bd9325d4a284710f146ed40536
Patrick Reynolds <cpreynolds@gmail.com>
2013-04-05 16:40:45 -0400

    ENH: Incrementing the version.

--- pydas/__init__.py
+++ pydas/__init__.py
@@ -24,7 +24,7 @@
 #
 ###############################################################################
 
-__version__ = '0.2.25'
+__version__ = '0.2.26'
 version = __version__
 
 from pydas.api import login, upload, download, add_item_upload_callback
