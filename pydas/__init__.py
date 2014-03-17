f70d3b8ec03d6d836c0e32c235f3b64fb653d909
Jamie Snape <jamie.snape@kitware.com>
2014-03-17 15:59:31 -0400

    ENH: Increment version

--- pydas/__init__.py
+++ pydas/__init__.py
@@ -24,6 +24,6 @@
 #
 ###############################################################################
 
-__version__ = '0.2.29'
+__version__ = '0.2.30'
 
 from pydas.api import login, upload, download, add_item_upload_callback
