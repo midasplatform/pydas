c231d53ddb8d3e7d1099ad85cd504b4ea8aecc68
Jamie Snape <jamie.snape@kitware.com>
2014-06-04 12:12:45 -0400

    Add method for creating a link

--- pydas/drivers.py
+++ pydas/drivers.py
@@ -589,6 +589,37 @@ class CoreDriver(BaseDriver):
         response = self.request('midas.item.searchbyname', parameters)
         return response['items']
 
+    def create_link(self, token, folder_id, url, **kwargs):
+        """Create a link bitstream.
+
+        :param token: A valid token for the user in question.
+        :param folder_id: The id of the folder in which to create a new item
+        that will contain the link. The new item will have the same name as the
+        URL unless an item name is supplied.
+        :param url: The URL of the link you will create, will be used as the
+        name of the bitstream and of the item unless an item name is supplied.
+        :param item_name: (optional)  The name of the newly created item, if
+        not supplied, the item will have the same name as the URL.
+        :param length: (optional) The length in bytes of the file to which the
+        link points.
+        :param checksum: (optional) The MD5 checksum of the file to which the
+        link points.
+        :returns: The item information of the item created.
+        """
+        parameters = dict()
+        parameters['token'] = token
+        parameters['folderid'] = folder_id
+        parameters['url'] = url
+        optional_keys = ['item_name', 'length', 'checksum']
+        for key in optional_keys:
+            if key in kwargs:
+                if key == 'item_name':
+                    parameters['itemname'] = kwargs[key]
+                    continue
+                parameters[key] = kwargs[key]
+        response = self.request('midas.link.create', parameters)
+        return response
+
     def generate_upload_token(self, token, item_id, filename, checksum=None):
         """Generate a token to use for upload.
 
