de801c5d1df199569c1617269ae745652f51f6cc
Jamie Snape <jamie.snape@kitware.com>
2014-08-15 12:49:30 -0400

    Add a method for searching by item and folder name

--- pydas/drivers.py
+++ pydas/drivers.py
@@ -589,6 +589,23 @@ class CoreDriver(BaseDriver):
         response = self.request('midas.item.searchbyname', parameters)
         return response['items']
 
+    def search_item_by_name_and_folder_name(self, name, folder_name, token=None):
+        """Return all items with a given name and parent folder name.
+
+        :param name: The name of the item to search by.
+        :param folder_name: The name of the parent folder to search by.
+        :param token: (optional) A valid token for the user in question.
+
+        :returns: A list of all items with the given name and parent folder name.
+        """
+        parameters = dict()
+        parameters['name'] = name
+        parameters['folderName'] = folder_name
+        if token:
+            parameters['token'] = token
+        response = self.request('midas.item.searchbynameandfoldername', parameters)
+        return response['items']
+
     def create_link(self, token, folder_id, url, **kwargs):
         """Create a link bitstream.
 
