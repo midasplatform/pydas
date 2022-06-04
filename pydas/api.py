midasplatform/pydas

commit 2741c1af4f6ad83bb6e5b7550b2eb33c6eb4e7af

Improve download_item to verify checksum to either skip or replace local file
The following scenario are now handled:

(1) Local file does not exist.

    Example of message:

      Creating file at /path/to/name.ext

(2) Local file exist and checksum match.

    Example of message:

      Skipping existing file at /path/to/name.ext

(3) Local file exist and checksum do NOT match.

    Example of message:

      Replacing file at /path/to/name.ext
         local md5 [9c3f42d8c6a94aae0da490dd9fb3a4e0]
        remote md5 [b248c663fdc9f13ae211b4a9eb603a2b]

pydas/api.py
@@ -657,7 +657,18 @@ def _download_item(item_id, path='.', item=None):
         print('ERROR: Failed to download item_id {0}'.format(item_id))
         return
     item_path = os.path.join(path, filename)
-    print('Creating file at {0}'.format(item_path))
+    if os.path.exists(item_path):
+        if not item:
+            item = session.communicator.item_get(session.token, item_id)
+        local_checksum = _streaming_file_md5(item_path)
+        remote_checksum = item['revisions'][-1]['bitstreams'][-1]['checksum']
+        if local_checksum == remote_checksum:
+            print('Skipping existing file at {0}'.format(item_path))
+            return
+        print('Replacing file at {0}\n   local md5 [{1}]\n  remote md5 [{2}]'.format(
+            item_path, local_checksum, remote_checksum))
+    else:
+        print('Creating file at {0}'.format(item_path))
     out_file = open(item_path, 'wb')
     for block in content_iter:
         out_file.write(block)

commit 9307275d3ecef1fdbe0cd8c238d2108858fdad98

Fix download_item to ensure item is always retrieved along with revisions
This commit fixes a regression introduced in 2741c1af4 (Improve download_item
to verify checksum to either skip or replace local file)

pydas/api.py
@@ -658,7 +658,8 @@ def _download_item(item_id, path='.', item=None):
         return
     item_path = os.path.join(path, filename)
     if os.path.exists(item_path):
-        if not item:
+        # Always retrieve item to ensure "revisions" data are available
+        if any([item is None, 'revision' not in item]):
             item = session.communicator.item_get(session.token, item_id)
         local_checksum = _streaming_file_md5(item_path)
         remote_checksum = item['revisions'][-1]['bitstreams'][-1]['checksum']